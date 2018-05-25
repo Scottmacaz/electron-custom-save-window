d605754341f47c9fbec2699e55414dc803061c97
scottmacaz <scottmacaz@yahoo.com>
2018-05-25 15:31:11 -0700

    going to try and setup angular cli

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,4 +6,7 @@ import { Component } from '@angular/core';
     styleUrls: [require('./app.component.scss')]
 })
 export class AppComponent {
+    onDownload() {
+        console.log("Works!")
+    }
 }
