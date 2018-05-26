2e8bcdb731c35cc84224630290829831c9bcbbac
scottmacaz <scottmacaz@yahoo.com>
2018-05-26 08:15:36 -0700

    Added readme and a new version of the ng client

---
+++ Readme.md
@@ -0,0 +1,21 @@
+# Custom Electron File Save Dialogs
+
+An angular application running in Electron that downloads a file from a server and then presents custom dialogs
+so that the file can only be saved to a USB drive.
+
+## Getting Started
+
+Both the Electron / Angular client and the node server must be running.
+
+### Installing
+
+Run 'npm install' in both the client and the server folders.
+
+## Runing the server.
+'npm start" in the server directory.
+
+## Running the client.
+The client requires two steps:
+1) Start the angular application with 'npm start'.
+2) Start the electron application with 'npm run start-electron'.
+
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,15 +1,16 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {AppComponent} from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
-    imports: [
-        BrowserModule
-    ],
-    declarations: [
-        AppComponent
-    ],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
